725bbea3bb6b4534937fa6b6138ed76ac9f751ad
samaila-bashir <developer.chatto@gmail.com>
2024-09-21 09:20:06 +0100

    removed sign in using email and password

--- src/app/modules/auth/components/Login.tsx
+++ src/app/modules/auth/components/Login.tsx
@@ -1,6 +1,4 @@
 import * as Yup from "yup";
-import clsx from "clsx";
-import { Link } from "react-router-dom";
 import { useFormik } from "formik";
 import { toAbsoluteUrl } from "../../../../_metronic/helpers";
 import { useDispatch, useSelector } from "react-redux";
@@ -35,9 +33,7 @@ export function Login() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { loading, error } = useSelector(
-    (state: RootState) => state.authentication
-  );
+  const { error } = useSelector((state: RootState) => state.authentication);
 
   const formik = useFormik({
     initialValues,
@@ -52,28 +48,28 @@ export function Login() {
 
   return (
     <form
-      className="form w-100"
+      className='form w-100'
       onSubmit={formik.handleSubmit}
       noValidate
-      id="kt_login_signin_form"
+      id='kt_login_signin_form'
     >
       {/* begin::Heading */}
-      <div className="text-center mb-11">
-        <h1 className="text-gray-900 fw-bolder mb-3">Sign In</h1>
-        <div className="text-gray-500 fw-semibold fs-6">
+      <div className='text-center mb-11'>
+        <h1 className='text-gray-900 fw-bolder mb-3'>Sign In</h1>
+        <div className='text-gray-500 fw-semibold fs-6'>
           Using your social accounts
         </div>
       </div>
       {/* begin::Heading */}
 
       {/* begin::Login options */}
-      <div className="row g-3 mb-9">
+      <div className='row g-3 mb-9'>
         {/* begin::Col */}
-        <div className="col-md-6">
+        <div className='col-md-6'>
           {/* begin::Google link */}
           <a
-            href="#"
-            className="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100"
+            href='#'
+            className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100'
             onClick={() => {
               dispatch({
                 type: SAGA_ACTIONS.GOOGLE_LOGIN,
@@ -82,9 +78,9 @@ export function Login() {
             }}
           >
             <img
-              alt="Logo"
+              alt='Logo'
               src={toAbsoluteUrl("media/svg/brand-logos/google-icon.svg")}
-              className="h-15px me-3"
+              className='h-15px me-3'
             />
             Sign in with Google
           </a>
@@ -93,21 +89,21 @@ export function Login() {
         {/* end::Col */}
 
         {/* begin::Col */}
-        <div className="col-md-6">
+        <div className='col-md-6'>
           {/* begin::Google link */}
           <a
-            href="#"
-            className="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100"
+            href='#'
+            className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100'
           >
             <img
-              alt="Logo"
+              alt='Logo'
               src={toAbsoluteUrl("media/svg/brand-logos/facebook-4.svg")}
-              className="theme-light-show h-15px me-3"
+              className='theme-light-show h-15px me-3'
             />
             <img
-              alt="Logo"
+              alt='Logo'
               src={toAbsoluteUrl("media/svg/brand-logos/facebook-4.svg")}
-              className="theme-dark-show h-15px me-3"
+              className='theme-dark-show h-15px me-3'
             />
             Sign in with Facebook
           </a>
@@ -117,104 +113,11 @@ export function Login() {
       </div>
       {/* end::Login options */}
 
-      {/* begin::Separator */}
-      <div className="separator separator-content my-14">
-        <span className="w-125px text-gray-500 fw-semibold fs-7">
-          Or with email
-        </span>
-      </div>
-      {/* end::Separator */}
-
       {error && (
-        <div className="mb-lg-15 alert alert-danger">
-          <div className="alert-text font-weight-bold">{error}</div>
+        <div className='mb-lg-15 alert alert-danger'>
+          <div className='alert-text font-weight-bold'>{error}</div>
         </div>
       )}
-
-      {/* begin::Form group */}
-      <div className="fv-row mb-8">
-        <label className="form-label fs-6 fw-bolder text-gray-900">Email</label>
-        <input
-          placeholder="Email"
-          {...formik.getFieldProps("email")}
-          className={clsx(
-            "form-control bg-transparent",
-            { "is-invalid": formik.touched.email && formik.errors.email },
-            {
-              "is-valid": formik.touched.email && !formik.errors.email,
-            }
-          )}
-          type="email"
-          name="email"
-          autoComplete="off"
-        />
-        {formik.touched.email && formik.errors.email && (
-          <div className="fv-plugins-message-container">
-            <span role="alert">{formik.errors.email}</span>
-          </div>
-        )}
-      </div>
-      {/* end::Form group */}
-
-      {/* begin::Form group */}
-      <div className="fv-row mb-3">
-        <label className="form-label fw-bolder text-gray-900 fs-6 mb-0">
-          Password
-        </label>
-        <input
-          type="password"
-          autoComplete="off"
-          {...formik.getFieldProps("password")}
-          className={clsx(
-            "form-control bg-transparent",
-            {
-              "is-invalid": formik.touched.password && formik.errors.password,
-            },
-            {
-              "is-valid": formik.touched.password && !formik.errors.password,
-            }
-          )}
-        />
-        {formik.touched.password && formik.errors.password && (
-          <div className="fv-plugins-message-container">
-            <div className="fv-help-block">
-              <span role="alert">{formik.errors.password}</span>
-            </div>
-          </div>
-        )}
-      </div>
-      {/* end::Form group */}
-
-      {/* begin::Wrapper */}
-      <div className="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
-        <div />
-
-        {/* begin::Link */}
-        <Link to="/auth/forgot-password" className="link-primary">
-          Forgot Password ?
-        </Link>
-        {/* end::Link */}
-      </div>
-      {/* end::Wrapper */}
-
-      {/* begin::Action */}
-      <div className="d-grid mb-10">
-        <button
-          type="submit"
-          id="kt_sign_in_submit"
-          className="btn btn-primary"
-          disabled={loading}
-        >
-          {!loading && <span className="indicator-label">Login</span>}
-          {loading && (
-            <span className="indicator-progress" style={{ display: "block" }}>
-              Please wait...
-              <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
-            </span>
-          )}
-        </button>
-      </div>
-      {/* end::Action */}
     </form>
   );
 }
